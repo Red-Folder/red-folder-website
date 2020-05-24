b427ac6be2b1c3a7759bf7b14852758aa637a77a
Red Folder <markbryantaylor@yahoo.co.uk>
2020-05-24 17:25:55 +0100

    Added Storybook

---
+++ src/components/buttons/github-button.stories.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import GithubButton from "./github-button"
+
+export default {
+  title: "GithubButton",
+}
+
+export const normal = () => <GithubButton />
---
+++ src/components/buttons/github-button.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const GithubButton = () => {
+  return <div>Hello</div>
+}
+
+export default GithubButton
